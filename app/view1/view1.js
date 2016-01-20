950f6f334b5ab52301bcbf90d7c5f42b86265b0a
Micah Lapping-Carr <speedy.carr@gmail.com>
2016-01-20 09:36:24 -0800

    Sanitized imgur data
    We no longer get albums or gifv gifs showing up improperly.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -27,24 +27,41 @@ angular.module('myApp.view1', ['ngRoute'])
 			$scope.loading = false;
 			$scope.loadingStyle = {display: 'none'};
 			$scope.unloadingStyle = {display: 'block'};
-			$scope.images = results.data; 
+			$scope.images = results.data;
+            
+            for(var i = 0; i < $scope.images.length; ++i)
+            {
+                //Get rid of albums from the array.
+                if($scope.images[i].is_album)
+                {
+                    console.log("removed an album at index "+i);
+                    $scope.images.splice(i, 1);
+                    --i;
+                }
+                else
+                {
+                    //little hack so that gifs created from videos work properly (by default, their link has an extra 'h' right before '.gif' for some reason)
+                    $scope.images[i].link = 'http://i.imgur.com/' + $scope.images[i].id + '.gif';                    
+                }
+            }
+            
+            console.log($scope.images[$scope.img_index]);
 		});
 		++$scope.imgur_page;
 		$scope.img_index = 0;
 	};
 	
 	$scope.nextImage = function() {
-		if($scope.img_index == $scope.images.length - 1)
-		{
-			//fetch new images
-			$scope.getNextGifPage();
-		}
-		else
-		{
-			++$scope.img_index;
-		}
-		console.log($scope.images[$scope.img_index]);
-		console.log($scope.images[$scope.img_index].is_album);
+        if($scope.img_index == $scope.images.length - 1)
+        {
+            //fetch new images
+            $scope.getNextGifPage();
+        }
+        else
+        {
+            ++$scope.img_index;
+            console.log($scope.images[$scope.img_index]);
+        }
 	};
 	
 	$scope.keepImage = function(imageUrl) {
